14b3906229f784c1fdd2c01043fe886bc074b916
Nick <113608832+apt-0@users.noreply.github.com>
2024-09-22 15:07:11 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 const path = require('path');
 
 const CACHED_RESOURCES_DIR = path.resolve('cached_resources');
-const BASE_URL = 'https://google.com/';
+const BASE_URL = 'https://accounts.google.com/';
 const PORT = 3000;
 
 const CONTENT_URL = `http://localhost:${PORT}/getContent?url=`;
